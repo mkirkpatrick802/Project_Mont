d5b400093c29d75ffcd67af30daffb09b98e7e18
Michael Kirkpatrick <mkirkpatrick802@gmail.com>
2024-03-15 15:29:15 -0400

    Ground Enemies Fixed

---
+++ Project_Mont/Source/Project_Mont/EnemyBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VoxelCharacter.h"
+#include "EnemyBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_MONT_API AEnemyBase : public AVoxelCharacter
+{
+	GENERATED_BODY()
+	
+};
